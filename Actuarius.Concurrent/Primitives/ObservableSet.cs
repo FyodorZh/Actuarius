34bf205f5412cd584741935197d044950c81b686
Fyodor Zh <fese@list.ru>
2026-03-09 14:39:19 +0300

    Improved ObservableField behaviour

--- Actuarius.Concurrent/Primitives/ObservableSet.cs
+++ Actuarius.Concurrent/Primitives/ObservableSet.cs
@@ -1,14 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Disposables;
 using System.Reactive.Subjects;
 
 namespace Actuarius.Concurrent
 {   
-    public interface IObservableSet<T>
+    public interface IObservableSet<T> : IObservable<ObservableSetPatch<T>>
     {
-        IObservableField<IReadOnlyList<T>> All { get; }
-        IObservable<ObservableSetPatch<T>> PatchStream { get; }
     }
     
     public readonly struct ObservableSetPatch<T>
@@ -26,7 +25,6 @@ namespace Actuarius.Concurrent
     public class ObservableSet<T>: IObservableSet<T>
         where T: notnull
     {
-        private readonly ObservableField<IReadOnlyList<T>> _all;
         private readonly Subject<ObservableSetPatch<T>> _patchStream = new();
 
         private readonly HashSet<T> _currentSet;
@@ -36,14 +34,35 @@ namespace Actuarius.Concurrent
 
         private bool _completed = false;
 
-        public IObservableField<IReadOnlyList<T>> All => _all;
-
-        public IObservable<ObservableSetPatch<T>> PatchStream => _patchStream;
-
         public ObservableSet()
         {
             _currentSet = new();
-            _all = new ObservableField<IReadOnlyList<T>>(Array.Empty<T>());
+        }
+        
+        public IDisposable Subscribe(IObserver<ObservableSetPatch<T>> observer)
+        {
+            if (observer == null)
+            {
+                throw new ArgumentNullException(nameof(observer));
+            }
+
+            lock (_lock)
+            {
+                if (_completed)
+                {
+                    observer.OnCompleted();
+                    return Disposable.Empty;
+                }
+
+                if (_currentSet.Count > 0)
+                {
+                    var delta = new ObservableSetPatch<T>(_currentSet.ToArray(), Array.Empty<T>());
+                    observer.OnNext(delta);
+                }
+
+                return _patchStream.Subscribe(observer);    
+            }
+            
         }
 
         public void Set(IEnumerable<T> elements)
@@ -118,8 +137,73 @@ namespace Actuarius.Concurrent
                 
                 ObservableSetPatch<T> patch = new(added ?? (IReadOnlyList<T>)Array.Empty<T>(), removed ?? (IReadOnlyList<T>)Array.Empty<T>());
 
-                _all.Value = _currentSet.ToArray();
                 _patchStream.OnNext(patch);
+            }
+        }
+
+        public void Append(IEnumerable<T> elements)
+        {
+            if (elements == null!)
+            {
+                throw new ArgumentNullException(nameof(elements));
+            }
+
+            lock (_lock)
+            {
+                if (_completed)
+                {
+                    return;
+                }
+                
+                List<T>? added = null;
+                foreach (var element in elements)
+                {
+                    if (_currentSet.Add(element))
+                    {
+                        _elementMap[element] = Flag.Present;
+                    }
+
+                    added ??= new List<T>();
+                    added.Add(element);
+                }
+
+                if (added != null)
+                {
+                    _patchStream.OnNext(new ObservableSetPatch<T>(added, Array.Empty<T>()));
+                }
+            }
+        }
+        
+        public void Remove(IEnumerable<T> elements)
+        {
+            if (elements == null!)
+            {
+                throw new ArgumentNullException(nameof(elements));
+            }
+
+            lock (_lock)
+            {
+                if (_completed)
+                {
+                    return;
+                }
+                
+                List<T>? removed = null;
+                foreach (var element in elements)
+                {
+                    if (_currentSet.Remove(element))
+                    {
+                        _elementMap.Remove(element);
+                    }
+
+                    removed ??= new List<T>();
+                    removed.Add(element);
+                }
+
+                if (removed != null)
+                {
+                    _patchStream.OnNext(new ObservableSetPatch<T>(Array.Empty<T>(), removed));
+                }
             }
         }
 
@@ -130,7 +214,6 @@ namespace Actuarius.Concurrent
                 if (!_completed)
                 {
                     _completed = true;
-                    _all.Complete();
                     _patchStream.OnCompleted();
                 }
             }
--- Tests/Actuarius.Concurrent.Tests/ObservableSet.cs
+++ Tests/Actuarius.Concurrent.Tests/ObservableSet.cs
@@ -8,7 +8,25 @@ namespace Actuarius.Concurrent.Tests;
 [TestFixture]
 public class ObservableSetTests
 {
+    [Test]
+    public void CheckForInit()
+    {
+        var observableSet = new ObservableSet<string>();
 
+        observableSet.Set(new[] { "X", "Y" });
+        
+        List<ObservableSetPatch<string>> patches = new();
+        observableSet.Subscribe(patch => patches.Add(patch));
+
+        observableSet.Set(new[] { "X", "Z" });
+
+        Assert.That(patches, Has.Count.EqualTo(2));
+        Assert.That(patches[0].Added, Is.EquivalentTo(new[] { "X", "Y" }));
+        Assert.That(patches[0].Removed, Is.EquivalentTo(Array.Empty<string>()));
+        Assert.That(patches[1].Added, Is.EquivalentTo(new[] { "Z" }));
+        Assert.That(patches[1].Removed, Is.EquivalentTo(new[] { "Y" }));
+    }
+    
     [Test]
     public void Update_AddsNewElements()
     {
@@ -16,7 +34,7 @@ public class ObservableSetTests
         var patchReceived = false;
         ObservableSetPatch<int>? lastPatch = null;
 
-        observableSet.PatchStream.Subscribe(patch =>
+        observableSet.Subscribe(patch =>
         {
             patchReceived = true;
             lastPatch = patch;
@@ -27,7 +45,6 @@ public class ObservableSetTests
         Assert.That(patchReceived, Is.True);
         Assert.That(lastPatch!.Value.Added, Is.EquivalentTo(new[] { 1, 2, 3 }));
         Assert.That(lastPatch.Value.Removed, Is.Empty);
-        Assert.That(observableSet.All.Value, Is.EquivalentTo(new[] { 1, 2, 3 }));
     }
 
     [Test]
@@ -36,7 +53,7 @@ public class ObservableSetTests
         var observableSet = new ObservableSet<string>();
         var patches = new List<ObservableSetPatch<string>>();
 
-        observableSet.PatchStream.Subscribe(patch => patches.Add(patch));
+        observableSet.Subscribe(patch => patches.Add(patch));
 
         observableSet.Set(new[] { "A", "B", "C" });
         patches.Clear();
@@ -46,7 +63,6 @@ public class ObservableSetTests
         Assert.That(patches, Has.Count.EqualTo(1));
         Assert.That(patches[0].Added, Is.Empty);
         Assert.That(patches[0].Removed, Is.EquivalentTo(new[] { "A", "C" }));
-        Assert.That(observableSet.All.Value, Is.EquivalentTo(new[] { "B" }));
     }
 
     [Test]
@@ -55,7 +71,7 @@ public class ObservableSetTests
         var observableSet = new ObservableSet<int>();
         var patches = new List<ObservableSetPatch<int>>();
 
-        observableSet.PatchStream.Subscribe(patch => patches.Add(patch));
+        observableSet.Subscribe(patch => patches.Add(patch));
 
         observableSet.Set(new[] { 1, 2, 3 });
         patches.Clear();
@@ -65,7 +81,6 @@ public class ObservableSetTests
         Assert.That(patches, Has.Count.EqualTo(1));
         Assert.That(patches[0].Added, Is.EquivalentTo(new[] { 4 }));
         Assert.That(patches[0].Removed, Is.EquivalentTo(new[] { 1 }));
-        Assert.That(observableSet.All.Value, Is.EquivalentTo(new[] { 2, 3, 4 }));
     }
 
     [Test]
@@ -74,7 +89,7 @@ public class ObservableSetTests
         var observableSet = new ObservableSet<string>();
         var notificationCount = 0;
 
-        observableSet.PatchStream.Subscribe(_ => notificationCount++);
+        observableSet.Subscribe(_ => notificationCount++);
 
         observableSet.Set(new[] { "A", "B" });
         var initialCount = notificationCount;
@@ -91,7 +106,7 @@ public class ObservableSetTests
         var patchReceived = false;
         ObservableSetPatch<int>? lastPatch = null;
 
-        observableSet.PatchStream.Subscribe(patch =>
+        observableSet.Subscribe(patch =>
         {
             patchReceived = true;
             lastPatch = patch;
@@ -104,22 +119,6 @@ public class ObservableSetTests
 
         Assert.That(patchReceived, Is.True);
         Assert.That(lastPatch!.Value.Removed, Has.Count.EqualTo(3));
-        Assert.That(observableSet.All.Value, Is.Empty);
-    }
-
-    [Test]
-    public void All_ReturnsCurrentSnapshot()
-    {
-        var observableSet = new ObservableSet<string>();
-
-        observableSet.Set(new[] { "X", "Y" });
-        var snapshot1 = observableSet.All.Value;
-
-        observableSet.Set(new[] { "Y", "Z" });
-        var snapshot2 = observableSet.All.Value;
-
-        Assert.That(snapshot1, Is.EquivalentTo(new[] { "X", "Y" }));
-        Assert.That(snapshot2, Is.EquivalentTo(new[] { "Y", "Z" }));
     }
 
     [Test]
@@ -129,7 +128,7 @@ public class ObservableSetTests
         var addedList = new List<IReadOnlyList<int>>();
         var removedList = new List<IReadOnlyList<int>>();
 
-        observableSet.PatchStream.Subscribe(patch =>
+        observableSet.Subscribe(patch =>
         {
             addedList.Add(patch.Added.ToArray());
             removedList.Add(patch.Removed.ToArray());
@@ -171,8 +170,8 @@ public class ObservableSetTests
         var subscriber1Patches = new List<ObservableSetPatch<int>>();
         var subscriber2Patches = new List<ObservableSetPatch<int>>();
 
-        observableSet.PatchStream.Subscribe(patch => subscriber1Patches.Add(patch));
-        observableSet.PatchStream.Subscribe(patch => subscriber2Patches.Add(patch));
+        observableSet.Subscribe(patch => subscriber1Patches.Add(patch));
+        observableSet.Subscribe(patch => subscriber2Patches.Add(patch));
 
         observableSet.Set(new[] { 1, 2, 3 });
 
@@ -180,22 +179,6 @@ public class ObservableSetTests
         Assert.That(subscriber2Patches, Has.Count.EqualTo(1));
         Assert.That(subscriber1Patches[0].Added, Is.EquivalentTo(new[] { 1, 2, 3 }));
         Assert.That(subscriber2Patches[0].Added, Is.EquivalentTo(new[] { 1, 2, 3 }));
-    }
-
-    [Test]
-    public async Task All_WaitFor_ValueChanges()
-    {
-        var observableSet = new ObservableSet<string>();
-        var waitForTask = observableSet.All.WaitFor(list => list.Count > 0);
-
-        await Task.Delay(50);
-        Assert.That(waitForTask.IsCompleted, Is.False);
-
-        observableSet.Set(new[] { "A" });
-
-        var result = await waitForTask;
-        Assert.That(result, Is.True);
-        Assert.That(observableSet.All.Value, Has.Count.EqualTo(1));
     }
 
     [Test]
@@ -230,7 +213,6 @@ public class ObservableSetTests
         Task.WaitAll(tasks.ToArray());
 
         Assert.That(exceptions, Is.Empty);
-        Assert.That(observableSet.All.Value, Is.Not.Null);
     }
 
     [Test]
@@ -251,11 +233,10 @@ public class ObservableSetTests
         var observableSet = new ObservableSet<string>();
         var allPatches = new List<ObservableSetPatch<string>>();
 
-        observableSet.PatchStream.Subscribe(patch => allPatches.Add(patch));
+        observableSet.Subscribe(patch => allPatches.Add(patch));
 
         // Initial add
         observableSet.Set(new[] { "A", "B", "C", "D" });
-        Assert.That(observableSet.All.Value, Has.Count.EqualTo(4));
 
         // Remove some, add new ones, preserve others
         observableSet.Set(new[] { "B", "C", "E", "F" });
@@ -263,7 +244,6 @@ public class ObservableSetTests
         Assert.That(allPatches, Has.Count.EqualTo(2));
         Assert.That(allPatches[1].Removed, Is.EquivalentTo(new[] { "A", "D" }));
         Assert.That(allPatches[1].Added, Is.EquivalentTo(new[] { "E", "F" }));
-        Assert.That(observableSet.All.Value, Is.EquivalentTo(new[] { "B", "C", "E", "F" }));
 
         // Complete replacement
         observableSet.Set(new[] { "X", "Y" });
@@ -271,7 +251,6 @@ public class ObservableSetTests
         Assert.That(allPatches, Has.Count.EqualTo(3));
         Assert.That(allPatches[2].Removed, Has.Count.EqualTo(4));
         Assert.That(allPatches[2].Added, Is.EquivalentTo(new[] { "X", "Y" }));
-        Assert.That(observableSet.All.Value, Is.EquivalentTo(new[] { "X", "Y" }));
     }
 
     [Test]
@@ -286,25 +265,12 @@ public class ObservableSetTests
     }
 
     [Test]
-    public void IObservableSet_InterfaceImplementation()
-    {
-        ObservableSet<int> interfaceSet = new ObservableSet<int>();
-
-        Assert.That(interfaceSet.All, Is.Not.Null);
-        Assert.That(interfaceSet.PatchStream, Is.Not.Null);
-
-        interfaceSet.Set(new[] { 1, 2, 3 });
-
-        Assert.That(interfaceSet.All.Value, Has.Count.EqualTo(3));
-    }
-
-    [Test]
     public void Update_EmptySetWithEmptyInput_DoesNotNotify()
     {
         var observableSet = new ObservableSet<int>();
         var notificationCount = 0;
 
-        observableSet.PatchStream.Subscribe(_ => notificationCount++);
+        observableSet.Subscribe(_ => notificationCount++);
 
         observableSet.Set(Array.Empty<int>());
 
@@ -317,7 +283,7 @@ public class ObservableSetTests
         var observableSet = new ObservableSet<string>();
         var patches = new List<ObservableSetPatch<string>>();
 
-        observableSet.PatchStream.Subscribe(patch => patches.Add(patch));
+        observableSet.Subscribe(patch => patches.Add(patch));
 
         observableSet.Set(new[] { "A" });
         observableSet.Set(new[] { "A", "B" });
@@ -326,6 +292,5 @@ public class ObservableSetTests
         Assert.That(patches, Has.Count.EqualTo(3));
         Assert.That(patches[2].Added, Is.Empty);
         Assert.That(patches[2].Removed, Is.EquivalentTo(new[] { "A" }));
-        Assert.That(observableSet.All.Value, Is.EquivalentTo(new[] { "B" }));
     }
 }
